9fa37fe29d713bd3502e119c58b07c52c1e5d444
Angel Motta <angelmotta@gmail.com>
2020-06-07 00:52:32 -0500

    itf-df

--- indexDB.py
+++ indexDB.py
@@ -12,11 +12,10 @@ tablaInicial = {'Termino': ['Clima', 'Biblioteca', 'Universidad', 'España', 'Li
                 'Q': [0, 1345, 1453, 0, 2133]
                 }
 
-df = pd.DataFrame(tablaInicial, columns=['Termino', 'Doc1', 'Doc2', 'Q'])
+df = pd.DataFrame(tablaInicial, columns=['Termino', 'Doc1', 'Doc2', 'Q'], index=['Clima', 'Biblioteca', 'Universidad', 'España', 'Libros'])
 
 df.to_csv(r'tabla_inicial.csv', index=False, header=True)
 
-print(df)
 
 def generateIndex(listTerm):
     indexDb = {}
@@ -42,12 +41,25 @@ def generateIndex(listTerm):
                         tf += 1
             if tf > 0:
                 indexDb[term][0] += 1  # Update Document Frequency
-                indexDb[term].append((document, tf))
+                indexDb[term].append([document, tf])
         fjson.close()
     return indexDb
 
 
 listTerm = ["espina", "corrupto", "fujimorista", "moral", "candidato", "miedo"]
 indexDb = generateIndex(listTerm)
+for term in indexDb:
+    print(term, "-->", indexDb[term])
+
+def genScore():
+    for term in indexDb:
+        idf = math.log(3/(indexDb[term][0]), 10)
+        for doc in range(1,len(indexDb[term])):
+            tf = indexDb[term][doc][1]
+            tfIdf = math.log(1+tf, 10) * idf
+            indexDb[term][0] = idf
+            indexDb[term][doc][1] = tfIdf
+print("----")
+genScore()
 for term in indexDb:
     print(term, "-->", indexDb[term])
