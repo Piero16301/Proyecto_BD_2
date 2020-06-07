e6323cc8dada360525dc590cc6566163190d2901
JeanleeRoy <jeanlee.barreto@utec.edu.pe>
2020-06-06 19:00:55 -0500

    basic flask front-end

---
+++ front.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+from tweets import tweets
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route("/search")
+def search():
+    consulta = request.args.get("query")
+    result = []
+    for re in tweets:
+        if consulta in re['text']:
+            result.append(re)
+    return render_template("resultado.html", consulta=consulta, result=result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ tweets.py
@@ -0,0 +1,5 @@
+tweets = [
+    {"id": "3253257", "text": "Lustrabotas usan escudo de madera para mantener distancia por COVID-19"},
+    {"id": "1233512", "text": "El presidente estadounidense, Donald Trump, anunció durante una rueda de prensa que la vacuna contra el COVID-19 está \"lista para usar\""},
+    {"id": "6464226", "text": "China solicita levantar sanciones unilaterales contra Venezuela para superar el Covid-19"}
+]
