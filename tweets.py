a5510ca4ae5fa32752da6fa388351d475fc0203b
Piero Angelo <piero.morales@utec.edu.pe>
2020-06-09 18:21:01 -0500

    Se implementa consulta tweet por ID

--- tweets.py
+++ tweets.py
@@ -1,5 +1,18 @@
-tweets = [
-    {"id": "3253257", "text": "Lustrabotas usan escudo de madera para mantener distancia por COVID-19"},
-    {"id": "1233512", "text": "El presidente estadounidense, Donald Trump, anunció durante una rueda de prensa que la vacuna contra el COVID-19 está \"lista para usar\""},
-    {"id": "6464226", "text": "China solicita levantar sanciones unilaterales contra Venezuela para superar el Covid-19"}
-]
+from twython import Twython, TwythonError
+
+def getTextTweet(tweetId):
+    # Credentials
+    CONSUMER_KEY = "WKtmu8QRdyTtKDeMirpDW50zj"
+    CONSUMER_SECRET = "G3pP3ZLJBcE7AHhiGAOsLPy06956oXUCbGstp0OIKM8EEH7312"
+    OAUTH_TOKEN = "104997748-7cCdw06UzvQI86C5bHskKl90qCPJi3vfbRTEef68"
+    OAUTH_TOKEN_SECRET = "ojBA7ZYhqrMrv6z0MqkonY74wOfQ9WZpNFloFgHykBib9"
+
+    # Constructor
+    twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+    # Query
+    tweet = twitter.show_status(id="1026814183042686976", tweet_mode='extended')
+    if tweet['retweeted_status'] is not None:
+        return tweet['retweeted_status']['full_text']
+    else:
+        return tweet['full_text']
